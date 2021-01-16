d4029bf18cd2e33408b58994489039b16be1e611
akasharkxx <akash.jaiswar41@gmail.com>
2021-01-16 15:57:01 +0530

    Completed Mini Game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,27 +10,56 @@ public class PlayerController : MonoBehaviour
     public bool isGrounded = true;
     public bool gameOver = false;
 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
     private Rigidbody playerRb;
     private Animator playerAnimator;
+    private AudioSource playerAudio;
 
     private int jumpTriggerHash;
+    private int deathBoolHash;
+    private int deathIntHash;
 
     private void Start()
     {
-
+        //Setup Animator
         playerAnimator = GetComponent<Animator>();
+        
+        //Setup Rigidbody and gravity
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+
+        //Setup Aduio Source
+        playerAudio = GetComponent<AudioSource>();
+
+        //Optimized Animator
         jumpTriggerHash = Animator.StringToHash("Jump_trig");
+        deathBoolHash = Animator.StringToHash("Death_b");
+        deathIntHash = Animator.StringToHash("DeathType_int");
     }
 
     private void Update()
     {
         if((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && isGrounded && !gameOver)
         {
+            //Add Jump force using Unity Physics
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            //Ensure to diable jump while in air
             isGrounded = false;
+            
+            //Dirt particle stop while jumping
+            dirtParticle.Stop();
+            
+            //Play Jump animation
             playerAnimator.SetTrigger(jumpTriggerHash);
+
+            //Play Jump Sound
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -39,13 +68,25 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+
+            //Dirt Particle if running on ground
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacles"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
-            playerAnimator.SetBool("Death_b", true);
-            playerAnimator.SetInteger("DeathType_int", 1);
+
+            //Particles
+            explosionParticle.Play();
+            dirtParticle.Stop();
+
+            //Play clip when crashes
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+
+            //Animation
+            playerAnimator.SetBool(deathBoolHash, true);
+            playerAnimator.SetInteger(deathIntHash, 1);
         }
     }
 }
